015a932459edb0d0865d0f89c9407e178d97b37d
Jose Galvez <jlgalvez77@gmail.com>
2025-03-18 00:55:24 +0100

    18/03/25-madrugada

---
+++ IntroduccionJava/src/Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Me llamo José Galvez");
+        System.out.println("Tengo 48 años");
+        System.out.println("Soy de España");
+    }
+}
---
+++ Variables/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 30;
+        double sueldo = 20300.50;
+        String nombre = "José";
+
+        System.out.println(edad);
+        System.out.println(sueldo);
+        System.out.println(nombre);
+
+        edad = 48;
+        System.out.println(edad);
+    }
+}
